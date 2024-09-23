7fb5a3e3bfdd47d0a95d1af845b65de362d7fc2c
Kalwaghesanika <kalwaghesanika@gmail.com>
2024-09-24 00:28:46 +0530

    Add files via upload
    This C++ program demonstrates the distinction between keywords and identifiers, showcasing their usage in variable declaration, control flow, and program structure. Keywords, reserved by the language, include int, float, bool, if, else, return, and main. Identifiers, chosen by the programmer, include myVariable, anotherVariable, and isTrue. The program declares and initializes variables of different data types (int, float, bool) and prints their values. It also uses the bool variable isTrue in an if-else statement, illustrating control flow. Finally, the program highlights the special function main as the entry point and utilizes the return statement to indicate successful execution. By combining these elements, the program illustrates fundamental C++ concepts, including variable declaration, data types, control structures, and program organization.

---
+++ define and uses of identifiers and keywords.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  //Keywords are reserved by the language and cannot be used as identifiers
+  int myVariable = 10; //'int' is a keyword,'myVariable' is an identifiers
+  float anotherVariable = 20.5f; //'float' is a keyword,'anotherVariable' is an identifiers
+  bool isTrue = true; //'bool' is a keyword,'isTrue' is an identifiers
+  
+  //print the values of the identifiers
+  cout << "The value of myVariable is: " << myVariable << endl;
+  cout << "The value of anotherVariable is: " << anotherVariable << endl;
+  cout << "The value of isTrue is: " << (isTrue?"true":"false") << endl;
+  
+  //Demonstrate the use of a keyword in control flow
+  if (isTrue) {
+     cout << "The condition is true!" << endl;
+  }else{
+     cout << "The condition is false!" << endl;
+  }
+  
+  //Keywords are used in defining the structure of the program
+  //'main' is a special function name that serves as the entry point of the program
+  return 0; //'return' is a Keywords
+}
+  
