858bc08390119222411fb971518364857e83aa69
Kalwaghesanika <kalwaghesanika@gmail.com>
2024-10-19 13:35:27 +0530

    Add files via upload
    This C++ program demonstrates the round function from the cmath library, which rounds a floating-point number to the nearest integer. The program includes necessary libraries: cmath for mathematical functions and iostream for input/output operations. In the main function, two examples illustrate the round function's behavior. When given 6.9, round returns 7 because 6.9 is closer to 7 than 6. Conversely, when given 6.4, round returns 6 because 6.4 is closer to 6 than 7.

---
+++ Rounding.cpp
@@ -0,0 +1,9 @@
+#include<cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout<<"Rounding 6.9 gives:"<<round(6.9)<<endl;
+  cout<<"Rounding 6.4 gives:"<<round(6.4)<<endl;
+  return 0;
+ }
