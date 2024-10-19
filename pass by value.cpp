cc323a9d6bb35f89e908bb22c82264c5e1b622a6
Kalwaghesanika <kalwaghesanika@gmail.com>
2024-10-19 18:52:46 +0530

    Add files via upload
    This C++ program demonstrates pass-by-value for strings, where a copy of the original string is passed to the printString function. In the main function, a string variable myString is initialized with "Good morning" and passed to printString. Within printString, a local copy of myString is created and printed to the console using cout. The program outputs "Good morning" to the console, illustrating a simple example of function usage and string passing

---
+++ pass by value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void printString(string str) {
+    cout << str << endl;
+}
+
+int main() {
+    string myString = "Good morning";
+    printString(myString);
+    return 0;
+}
+
