0e906b2665a754b769f7f1ae65fa7f0cdd833605
Kalwaghesanika <kalwaghesanika@gmail.com>
2024-10-19 19:13:48 +0530

    Add files via upload
    This C++ program demonstrates a simple function call with no parameters and no return value. The request function, declared with void return type, prints a message "Please give me a glass of water" to the console. In the main function, request is called, executing its code and displaying the message.

---
+++ Function with no parameters and no return value (1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Function definition with no parameters and no return value
+void request() {
+    cout << "Please give me a glass of water" << endl;
+}
+
+int main() {
+    // Function call
+    request();
+    return 0;
+}
