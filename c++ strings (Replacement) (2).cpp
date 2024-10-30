eaa571d0335d1931007d3d500f64790fb36961f7
Kalwaghesanika <kalwaghesanika@gmail.com>
2024-10-30 09:44:16 +0530

    Add files via upload
    This C++ program demonstrates string manipulation using the std::string class from the C++ Standard Library. It initializes a string variable text with the value "Hello World". The program then searches for the substring "World" within text using the find method, which returns the position of the substring if found, or string::npos if it is not found. If "World" is found (i.e., pos is not equal to string::npos), the program uses the replace method to substitute "World" with "Universe" at the found position. Finally, it outputs the modified string, displaying "Replaced: Hello Universe". This program effectively illustrates how to locate and replace substrings within a string in C++.

---
+++ c++ strings (Replacement) (2).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string text = "Hello World";
+size_t pos = text.find("World");
+if (pos != string::npos) {
+text.replace(pos, 5, "Universe"); // Replaces "World" with "Universe"
+}
+cout << "Replaced: " << text << endl;
+return 0;
+}
+
