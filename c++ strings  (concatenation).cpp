c3c21f91d356c92875be4ac691618f3de1999b54
Kalwaghesanika <kalwaghesanika@gmail.com>
2024-10-30 09:49:50 +0530

    Add files via upload
    This C++ program demonstrates string concatenation using the std::string class. It initializes two string variables, str1 with the value "Good" and str2 with "Morning". The program then concatenates these two strings with a space in between by using the + operator, resulting in the string "Good Morning". This concatenated result is stored in the variable result. Finally, the program outputs the combined string with the message "Concatenated: Good Morning". This example effectively illustrates how to combine multiple strings into one in C++.

---
+++ c++ strings  (concatenation).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string str1 = "Good";
+string str2 = "Morning";
+string result = str1 + " " + str2;
+cout << "Concatenated: " << result << endl;
+return 0;
+}
+
